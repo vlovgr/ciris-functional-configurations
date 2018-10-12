5682c41a6848bd28ffbac7a434adfeabf11837f2
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-10-12 16:06:42 +0100

    Add cowsay example implementation

--- src/main/scala/se/vlovgr/example/Main.scala
+++ src/main/scala/se/vlovgr/example/Main.scala
@@ -5,5 +5,10 @@ import se.vlovgr.example.cowsays._
 
 object Main extends IOApp {
   override def run(args: List[String]): IO[ExitCode] =
-    ???
+    for {
+      first <- cowsay[IO]("Please: stop using configuration files!",
+                          _.withEyes(Wired).withTongue("()").withMaxWidth(30))
+      second <- cowsay[IO](first, _.withNoMaxWidth.withCowFile(Dragon))
+      _ <- IO(println(second))
+    } yield ExitCode.Success
 }
