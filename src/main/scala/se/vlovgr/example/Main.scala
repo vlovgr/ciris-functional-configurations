81532177e0fab8e1bd99fa5eff11b05f77be6bce
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-11-09 15:44:54 +0000

    Add multiple environments and api key

--- src/main/scala/se/vlovgr/example/Main.scala
+++ src/main/scala/se/vlovgr/example/Main.scala
@@ -3,11 +3,12 @@ package se.vlovgr.example
 import cats.effect.{ExitCode, IO, IOApp}
 import se.vlovgr.example.config.Config
 import se.vlovgr.example.http.startHttpApi
+import cats.implicits._
 
 object Main extends IOApp {
   override def run(args: List[String]): IO[ExitCode] =
-    Config.load.fold(
-      errors => IO.raiseError(errors.toException),
-      config => startHttpApi[IO](config)
-    )
+    Config.load.flatMap { config =>
+      IO(println(config.show)) >>
+        startHttpApi[IO](config)
+    }
 }
--- src/main/scala/se/vlovgr/example/config/Config.scala
+++ src/main/scala/se/vlovgr/example/config/Config.scala
@@ -1,38 +1,75 @@
 package se.vlovgr.example.config
 
+import enumeratum._
+import cats.derived._
+import cats.effect.IO
+import cats.implicits._
+import cats.Show
 import ciris._
+import ciris.cats._
+import ciris.enumeratum._
+import ciris.cats.effect._
+import ciris.kubernetes._
 import ciris.enumeratum._
 import ciris.refined._
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.auto._
+import eu.timepit.refined.cats.refTypeShow
 import eu.timepit.refined.string.IPv4
 import eu.timepit.refined.types.net.UserPortNumber
+import se.vlovgr.example.config.AppEnv.{Production, Testing}
 
 import scala.concurrent.duration._
 
 final case class HttpConfig(
   host: String Refined IPv4,
   port: UserPortNumber,
-  idleTimeout: FiniteDuration
+  idleTimeout: FiniteDuration,
+  apiKey: Secret[ApiKey]
 )
 
 final case class Config(
   http: HttpConfig
 )
 
+sealed abstract class AppEnv extends EnumEntry
+
+object AppEnv extends Enum[AppEnv] {
+  case object Testing extends AppEnv
+  case object Production extends AppEnv
+
+  override def values: Vector[AppEnv] =
+    findValues.toVector
+}
+
 object Config {
-  def load: Either[ConfigErrors, Config] =
-    loadConfig(
-      env[Option[String Refined IPv4]]("HTTP_HOST"),
-      env[Option[UserPortNumber]]("HTTP_PORT"),
-      env[Option[FiniteDuration]]("HTTP_IDLE_TIMEOUT")
-    ) { (host, port, idleTimeout) =>
-      Config(
-        http = HttpConfig(
-          host = host.getOrElse("0.0.0.0"),
-          port = port.getOrElse(9000),
-          idleTimeout = idleTimeout.getOrElse(20.seconds)
+  implicit val showConfig: Show[Config] = {
+    implicit val finiteDurationShow: Show[FiniteDuration] =
+      Show.fromToString
+
+    import auto.showPretty._
+    semi.showPretty
+  }
+
+  def load: IO[Config] =
+    for {
+      apiClient <- defaultApiClient[IO]
+      secret = secretInNamespace[IO]("secrets", apiClient)
+      config <- loadConfig(
+        envF[IO, AppEnv]("APP_ENV"),
+        envF[IO, Secret[ApiKey]]("API_KEY").orElse(secret("api-key"))
+      ) { (environment, apiKey) =>
+        Config(
+          http = HttpConfig(
+            host = "0.0.0.0",
+            port = 9000,
+            idleTimeout = environment match {
+              case Testing    => 10.seconds
+              case Production => 20.seconds
+            },
+            apiKey = apiKey
+          )
         )
-      )
-    }.result
+      }.orRaiseThrowable
+    } yield config
 }
